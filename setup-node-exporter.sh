2ebfb5ff7a4327862bbd05aeb04f413913533265
zhostvn <117628532+zhostvn@users.noreply.github.com>
2024-07-13 12:19:14 +0700

    Update setup-node-exporter.sh

--- setup-node-exporter.sh
+++ setup-node-exporter.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 clear
+rm -rf /etc/systemd/system/node_exporter.service
 wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
 
 tar -xzvf node_exporter-1.6.1.linux-amd64.tar.gz
