a252f48d52ed311f0c7f18c3e4d181ec5b5e959d
zhostvn <117628532+zhostvn@users.noreply.github.com>
2024-07-13 11:57:29 +0700

    Create setup-pve-exporter.sh

---
+++ setup-pve-exporter.sh
@@ -0,0 +1,37 @@
+apt install python3-pip -y
+rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED
+python3 -m pip install prometheus-pve-exporter
+
+mkdir /etc/prometheus
+touch /etc/prometheus/pve.yml
+
+
+tee -a /etc/prometheus/pve.yml > /dev/null <<EOT
+default:
+    user: root@pam
+    token_name: "root_prometheus"
+    token_value: "ce5b985c-cd27-4d1d-90a9-f77c5ec7e31b"
+    verify_ssl: false
+EOT
+
+
+tee -a /etc/systemd/system/pve_exporter.service > /dev/null <<EOT
+[Unit]
+Description=PVE Exporter
+After=network.target
+
+[Service]
+User=nodeusr
+Group=nodeusr
+Type=simple
+ExecStart=pve_exporter --collector.status
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+
+systemctl daemon-reload
+systemctl start pve_exporter
+systemctl enable pve_exporter
+systemctl status pve_exporter
